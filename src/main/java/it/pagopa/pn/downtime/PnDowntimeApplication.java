4a402b95ee4e5bc7f5885028211e5d10752df0ee
CSCMWS\gporro <giuseppe.porro@dxc.com>
2022-10-19 11:41:47 +0200

    fix unused lib and warning

--- src/main/java/it/pagopa/pn/downtime/PnDowntimeApplication.java
+++ src/main/java/it/pagopa/pn/downtime/PnDowntimeApplication.java
@@ -2,7 +2,6 @@ package it.pagopa.pn.downtime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
