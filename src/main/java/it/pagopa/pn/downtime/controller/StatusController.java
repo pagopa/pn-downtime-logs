15313ca777bdbb518ecd7d09867f046d65ee824b
angelaantonia.cea <angelaantonia.cea@dxc.com>
2022-10-26 10:12:33 +0200

    modified /healthcheck and /status endpoint

--- src/main/java/it/pagopa/pn/downtime/controller/StatusController.java
+++ src/main/java/it/pagopa/pn/downtime/controller/StatusController.java
@@ -1,21 +1,30 @@
 package it.pagopa.pn.downtime.controller;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RestController;
 
 import it.pagopa.pn.downtime.pn_downtime_logs.api.StatusApi;
+import it.pagopa.pn.downtime.pn_downtime_logs.model.PnStatusResponse;
+import it.pagopa.pn.downtime.service.DowntimeLogsService;
 
 @Validated
 @RestController
 public class StatusController implements StatusApi {
-
 	
-	@Override
-	public ResponseEntity<Void> healthcheck() {
-		return new ResponseEntity<>(HttpStatus.OK);
+	@Autowired
+	private DowntimeLogsService downtimeLogsService;
 
+	@Override
+	public ResponseEntity<PnStatusResponse> status() {
+		PnStatusResponse openIncidents = downtimeLogsService.currentStatus();
+		if (!openIncidents.getOpenIncidents().isEmpty()) {
+			return ResponseEntity.internalServerError().body(openIncidents);
+		} else
+			return ResponseEntity.ok(openIncidents);
 	}
+	
+
 	
 }
