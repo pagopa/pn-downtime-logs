a5bbfd6d1d4d71ed00da63667433569bb5f45c89
gparente3 <giovanni.parente@dxc.com>
2022-11-09 12:52:10 +0100

    Refactoring AppControllerAdvice

--- src/main/java/it/pagopa/pn/downtime/controller/DowntimeApiController.java
+++ src/main/java/it/pagopa/pn/downtime/controller/DowntimeApiController.java
@@ -1,14 +1,8 @@
 package it.pagopa.pn.downtime.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.info.BuildProperties;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.RequestEntity.BodyBuilder;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DowntimeApiController {
 	
-//	@Autowired
-//	BuildProperties buildProperties;
 	
 	@RequestMapping(path = "/downtime/v1/status", produces = { "application/json" }, method = RequestMethod.OPTIONS)
 	public ResponseEntity<BodyBuilder> currentStatus() {
@@ -46,10 +38,4 @@ public class DowntimeApiController {
 		return ResponseEntity.ok().allow(HttpMethod.GET, HttpMethod.OPTIONS).build();
 	}
 	
-//	@GetMapping(path = "/version", produces = { "application/json" })
-//	public ResponseEntity<Object> getVersion(){
-//		Map<String, String> mapVersion = new HashMap<>();
-//		mapVersion.put("version", buildProperties.getVersion());
-//		return ResponseEntity.ok().body(mapVersion);
-//	}
 }
--- src/main/java/it/pagopa/pn/downtime/util/Constants.java
+++ src/main/java/it/pagopa/pn/downtime/util/Constants.java
@@ -4,7 +4,9 @@ public class Constants {
 	
 	private Constants() {}
 
-	public static final String COG_ACTOKEN = "AccessToken";
-	public static final String COGNITO_CUSTOM_ATTRIBUTE_PREFIX = "custom:";
-
+	public static final String GENERIC_MESSAGE = "Errore nell'elaborazione della richiesta";
+	public static final String GENERIC_ENGLISH_MESSAGE = "An error occured during request elaboration";
+	public static final String GENERIC_BAD_REQUEST_ERROR_MESSAGE = "Informazioni non valide";
+	public static final String GENERIC_BAD_REQUEST_ERROR_ENGLISH_MESSAGE = "Informations not valid";
+	public static final String TRACE_ID_PLACEHOLDER = "trace_id";
 }
