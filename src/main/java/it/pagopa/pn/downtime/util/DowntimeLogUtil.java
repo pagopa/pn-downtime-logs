d0c332798ac65698ab0ff1f0c605637c62234c58
Melissa <melissadellorco1993@gmail.com>
2022-11-23 10:20:24 +0100

    Fixed gmtDate

--- src/main/java/it/pagopa/pn/downtime/util/DowntimeLogUtil.java
+++ src/main/java/it/pagopa/pn/downtime/util/DowntimeLogUtil.java
@@ -12,27 +12,27 @@ public class DowntimeLogUtil {
 
 	public static OffsetDateTime getGmtTimeFromOffsetDateTime(OffsetDateTime date) {
 
+		OffsetDateTime gmtDate = null;
+
 		if(date != null) {
 			Timestamp timestampDate = Timestamp.valueOf(date.atZoneSameInstant(ZoneOffset.UTC).toLocalDateTime());
 			Date newDate = new Date(timestampDate.getTime());
-			return newDate.toInstant().atOffset(ZoneOffset.UTC);
-		} else {
-			return null;
-		}
+			gmtDate = newDate.toInstant().atOffset(ZoneOffset.UTC);
 
+		}
+		return gmtDate;
 	}
 
 
 	public static OffsetDateTime getOffsetDateTimeFromGmtTime(OffsetDateTime gmtDate) {
 
+		OffsetDateTime date = null;
 		if(gmtDate != null) {
 			Timestamp timestampDate = Timestamp.valueOf(gmtDate.atZoneSameInstant(TimeZone.getDefault().toZoneId()).toLocalDateTime());
 			Date newDate = new Date(timestampDate.getTime());
-			return newDate.toInstant().atOffset(ZoneOffset.systemDefault().getRules().getOffset(Instant.now())); 
-		} else {
-			return null;
-		}
-
+			date = newDate.toInstant().atOffset(ZoneOffset.systemDefault().getRules().getOffset(Instant.now())); 
+		} 
+		return date;
 	}
 
 
