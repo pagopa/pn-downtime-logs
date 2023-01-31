1afd9a7bd236ee7e0b5b119cf3bbfe932783c438
acolucci <a.colucci@dxc.com>
2023-01-31 16:18:24 +0100

    fix vulnerabilità dipendenza swagger2

--- src/main/java/it/pagopa/pn/downtime/PnDowntimeApplication.java
+++ src/main/java/it/pagopa/pn/downtime/PnDowntimeApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class PnDowntimeApplication {
 
 	public static void main(String[] args) {
