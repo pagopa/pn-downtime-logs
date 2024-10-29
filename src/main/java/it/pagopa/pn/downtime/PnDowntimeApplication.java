293550f0d4c020ccf784ecfcd0b687a929f5a05c
marcellosattanttdata <marcello.satta@nttdata.com>
2024-10-29 16:37:25 +0100

    PN-10354: added Spring Analyzer for pn-downtime-logs

--- src/main/java/it/pagopa/pn/downtime/PnDowntimeApplication.java
+++ src/main/java/it/pagopa/pn/downtime/PnDowntimeApplication.java
@@ -1,13 +1,18 @@
 package it.pagopa.pn.downtime;
 
+import it.pagopa.pn.commons.configs.listeners.TaskIdApplicationListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class PnDowntimeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PnDowntimeApplication.class, args);
+		SpringApplication app = new SpringApplication(PnDowntimeApplication.class);
+		app.addListeners(new TaskIdApplicationListener());
+		app.run(args);
 	}
 
 }
---
+++ src/main/java/it/pagopa/pn/downtime/config/springbootcfg/SpringAnalyzerClientConfig.java
@@ -0,0 +1,7 @@
+package it.pagopa.pn.downtime.config.springbootcfg;
+
+import it.pagopa.pn.commons.configs.SpringAnalyzerConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringAnalyzerClientConfig extends SpringAnalyzerConfiguration {}
